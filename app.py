1f6793f063917aa23f166f9e381d360fae70a5c7
Wiktor <96666718+WiktorGruszczynski@users.noreply.github.com>
2022-12-28 15:07:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,17 @@
 import discord
-import json
-from audio import MediaPlayer
-import json
-
-settings_path = "src/settings.json"
-
-with open(settings_path ,"r") as file:
-    info = json.loads(file.read())
+import util
+import audio
 
 
-token = info["private"]["discord"]["token"]
+token = util.get_token()
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-
-bot = MediaPlayer(client=client)
-
-
-
-def get_prefix():
-    with open(settings_path ,"r") as file:
-        info = json.loads(file.read())
-    return info["prefix"]
-
-
-async def set_prefix(ctx):
-    new_prefix = ctx.content.split(" ")[1]
-
-    if len(new_prefix) == 1:
-        
-        with open("src/settings.json","r") as file:
-            info = json.loads(file.read())
-        
-        info["prefix"] = new_prefix
-
-        with open("src/settings.json","w") as file:
-            json.dump(info, file, indent=4)
-
-        await ctx.channel.send(f'"{new_prefix}" has been set as a new prefix!')
-
-    else:
-        await ctx.channel.send(f'You cannot set "{new_prefix}" as a prefix!')
+bot = audio.MediaPlayer(client=client)
 
 
 @client.event
 async def on_message(msg):
-    prefix = get_prefix()
+    prefix = util.get_prefix()
     if msg.author != client.user and msg.content.lower().startswith(prefix):
         msg.content: str = msg.content.removeprefix(prefix)
 
@@ -70,11 +37,9 @@ async def on_message(msg):
             await bot.disconnect(msg)
 
         elif msg.content.startswith("prefix"):
-            await set_prefix(msg)
+            await util.set_prefix(msg)
 
         elif msg.content.startswith("queue"):
             await bot.print_queue(msg)
-
-
 
 client.run(token)
